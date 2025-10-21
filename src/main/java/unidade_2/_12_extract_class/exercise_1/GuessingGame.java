c4e9955eb3a57d64d1dfe119b8eb4eebee9511ae
guilherme <guilhermefrancisco9@hotmail.com>
2025-10-21 08:58:46 -0300

    atividade 12 atualizada

--- src/main/java/unidade_2/_12_extract_class/exercise_1/GuessingGame.java
+++ src/main/java/unidade_2/_12_extract_class/exercise_1/GuessingGame.java
@@ -20,44 +20,38 @@ public class GuessingGame {
     }
 
     private void initializeGame() {
-        Random random = new Random();
-        secretNumber = random.nextInt(100) + 1; // Número entre 1 e 100
-        System.out.println("O jogo de adivinhar o número começou! Tente adivinhar um número entre 1 e 100.");
-        System.out.println("Você tem " + maxAttempts + " tentativas.");
-        this.currentAttempt = 0;
-        this.gameWon = false;
+        generateRandomNumber(100);
+
+        showInitialMessage();
+
+        setCurrentAttempt(0);
+
+        setGameWon(false);
     }
 
     public void startGame() {
         while (currentAttempt < maxAttempts && !gameWon) {
-            currentAttempt++;
+            increadAttempt();
             System.out.print("Tentativa " + currentAttempt + "/" + maxAttempts + ". Digite seu palpite: ");
 
-            int guess;
-            try {
-                guess = scanner.nextInt();
-            } catch (java.util.InputMismatchException e) {
-                System.out.println("Entrada inválida. Por favor, digite um número.");
-                scanner.next();
-                currentAttempt--;
-                continue;
-            }
+            int guess = readGuess();
 
-            if (guess == secretNumber) {
-                gameWon = true;
-                System.out.println("Parabéns! Você adivinhou o número " + secretNumber + " em " + currentAttempt + " tentativas!");
-            } else if (guess < secretNumber) {
-                System.out.println("Muito baixo! Tente novamente.");
-            } else {
-                System.out.println("Muito alto! Tente novamente.");
-            }
+            checkGuess(guess);
         }
 
+        checkGameWon();
+
+        playAgain();
+    }
+
+    private void checkGameWon() {
         if (!gameWon) {
             System.out.println("Fim de jogo! Você ficou sem tentativas.");
             System.out.println("O número secreto era: " + secretNumber);
         }
+    }
 
+    private void playAgain() {
         System.out.println("Deseja jogar novamente? (s/n)");
         String playAgain = scanner.next();
         if (playAgain.equalsIgnoreCase("s")) {
@@ -68,8 +62,74 @@ public class GuessingGame {
         }
     }
 
+    private void decreaseAttempt() {
+        this.currentAttempt--;
+    }
+
+    private void increadAttempt() {
+        this.currentAttempt++;
+    }
+
+    private void checkGuess(int guess) {
+        if (guess == secretNumber) {
+            gameWon = true;
+            System.out.println("Parabéns! Você adivinhou o número " + secretNumber + " em " + currentAttempt + " tentativas!");
+        } else if (guess < secretNumber) {
+            System.out.println("Muito baixo! Tente novamente.");
+        } else {
+            System.out.println("Muito alto! Tente novamente.");
+        }
+    }
+
+    private int readGuess() {
+        int guess = 0;
+        try {
+            guess = scanner.nextInt();
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Entrada inválida. Por favor, digite um número.");
+            scanner.next();
+            decreaseAttempt();
+        }
+
+        return guess;
+    }
+
+    private void generateRandomNumber(int numeroMaximo) {
+        Random random = new Random();
+        secretNumber = random.nextInt(numeroMaximo) + 1; // Número entre 1 e 100
+    }
+
+    private void showInitialMessage() {
+        System.out.println("O jogo de adivinhar o número começou! Tente adivinhar um número entre 1 e 100.");
+        System.out.println("Você tem " + maxAttempts + " tentativas.");
+    }
+
     public int getSecretNumber() { return secretNumber; }
     public int getMaxAttempts() { return maxAttempts; }
     public int getCurrentAttempt() { return currentAttempt; }
     public boolean isGameWon() { return gameWon; }
+
+    public void setSecretNumber(int secretNumber) {
+        this.secretNumber = secretNumber;
+    }
+
+    public void setMaxAttempts(int maxAttempts) {
+        this.maxAttempts = maxAttempts;
+    }
+
+    public void setCurrentAttempt(int currentAttempt) {
+        this.currentAttempt = currentAttempt;
+    }
+
+    public void setGameWon(boolean gameWon) {
+        this.gameWon = gameWon;
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
 }
